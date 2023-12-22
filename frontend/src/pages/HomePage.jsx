90afbfbdc2c2c0bc7641c103c30e83bd0d9297bc
Egert Kriisa <83175036+chello112@users.noreply.github.com>
2023-12-22 12:58:33 +0200

    frontend & backend pagination completed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,28 +1,35 @@
 import { Row, Col } from "react-bootstrap";
 import { useGetProductsQuery } from "../redux/features/productsApiSlice";
+import { useParams } from "react-router-dom";
+import Paginate from "../components/Paginate";
 import Product from "../components/Product";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 
 const HomePage = () => {
-  const { data: products, isLoading, isError } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+
+  const { data, isLoading, error } = useGetProductsQuery({
+    pageNumber,
+  });
 
   return (
     <>
       {isLoading ? (
         <Loader />
-      ) : isError ? (
-        <Message variant="danger">{isError?.data?.message || isError.error}</Message>
+      ) : error ? (
+        <Message variant="danger">{error?.data?.message || error.error}</Message>
       ) : (
         <>
           <h1>Latest Products</h1>
           <Row>
-            {products.map((product) => (
+            {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
             ))}
           </Row>
+          <Paginate pages={data.pages} page={data.page} />
         </>
       )}
     </>
--- frontend/src/pages/admin/ProductListPage.jsx
+++ frontend/src/pages/admin/ProductListPage.jsx
@@ -1,17 +1,21 @@
 import { LinkContainer } from "react-router-bootstrap";
 import { Table, Button, Row, Col } from "react-bootstrap";
 import { FaEdit, FaPlus, FaTrash } from "react-icons/fa";
+import { useParams } from "react-router-dom";
 import {
   useGetProductsQuery,
   useCreateProductMutation,
   useDeleteProductMutation,
 } from "../../redux/features/productsApiSlice";
 import { toast } from "react-toastify";
+import Paginate from "../../components/Paginate";
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
 
 const ProductListPage = () => {
-  const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+
+  const { data, isLoading, error, refetch } = useGetProductsQuery({ pageNumber });
 
   const [deleteProduct, { isLoading: loadingDelete }] = useDeleteProductMutation();
 
@@ -73,7 +77,7 @@ const ProductListPage = () => {
               </tr>
             </thead>
             <tbody>
-              {products.map((product) => (
+              {data.products.map((product) => (
                 <tr key={product._id}>
                   <td>{product._id}</td>
                   <td>{product.name}</td>
@@ -94,7 +98,7 @@ const ProductListPage = () => {
               ))}
             </tbody>
           </Table>
-          {/* PAGINATE PLACEHOLDER */}
+          <Paginate pages={data.pages} page={data.page} isAdmin={true} />
         </>
       )}
     </>
